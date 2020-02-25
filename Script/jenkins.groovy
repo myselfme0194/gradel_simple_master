31e3f5d2ca0be02e7e42be8e910310cff457216b
harsharameshh <48444072+harsharameshh@users.noreply.github.com>
2020-02-25 16:59:21 +0530

    Update jenkins.groovy

--- Script/jenkins.groovy
+++ Script/jenkins.groovy
@@ -1,36 +1,57 @@
-#!groovy
+node('windows') {
+	def buildVersion = "1.0.0.${env.BUILD_NUMBER}"
+	def artifactoryPublishRepo = 'Gradel_master'
 
-node ('windows'){
-  stage('checkout') {
-    git 'https://github.com/harsharameshh/artifactory.git'
-  }
-  stage('build'){
-     //  currentBuild.displayName = "1.10"
-      sh  " ./gradlew clean assemble"
-  }
-       stage('SonarQube analysis') {
-          withSonarQubeEnv() {
-      	 //  sh " ./gradlew sonarqube -Dsonar.projectKey=harsha_aig -Dsonar.host.url=http://34.219.184.2:9000  -Dsonar.login=admin -Dsonar.password=admin"
-          }
-      	}
-      	stage('run test'){
-      sh  " ./gradlew build"
-  }
-      	stage('artifactory')
-      	{
-      	  
-            sh "./gradlew artifactoryPublish  "  
-             def server = Artifactory.server 'JFROG'
-                 def uploadSpec = """{
-                 "files": [{
-                       "pattern": "path/",
-                       "target": "path/"
-                    }]
-                 }"""
-           
+	dir('jenkins') {
+		try {
+      
 
-                 server.upload(uploadSpec) 
-     
-            
-      	}
-      }
+			stage('Checkout') {
+				git url: 'https://github.com/harsharameshh/gradel_simple_master.git'
+			}
+
+			stage('Build') {
+				withEnv( "BUILD_VERSION=${buildVersion}"]) {
+					sh "./gradlew clean assemble"
+				}
+			}
+
+			stage('Run Tests') {
+				withEnv( "BUILD_VERSION=${buildVersion}"]) {
+					try {
+						sh "./gradlew build"
+					}
+					finally {
+						step([$class: 'JUnitResultArchiver', testResults: '**/test-results/**/*.xml'])
+					}
+				}
+			}
+
+			stage('Sonar scan') {
+				withEnv( "BUILD_VERSION=${buildVersion}", "GRADLE_OPTS=-Dsonar.host.url=${env.SONAR_URL} -Dsonar.login=${env.SONAR_CRED_ID}"]) {
+					sh "./gradlew sonarqube"
+				}
+			}
+
+			stage('Publish to Artifactory') {
+				withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'devopspipeline_artifactory_credential_id', usernameVariable: 'artifactoryUsername', passwordVariable: 'artifactoryPassword']]) {
+					withEnv(["JAVA_HOME=${jdkHome}", "BUILD_VERSION=${buildVersion}", "GRADLE_OPTS=-Dartifactory.publish.repo.user=${env.artifactoryUsername} -Dartifactory.publish.repo=${artifactoryPublishRepo} -Dartifactory.publish.repo.pwd=${env.artifactoryPassword}"]) {
+						sh "./gradlew artifactoryPublish"
+					}
+				}
+			}
+
+			stage('Notify') {
+				notify 'Success', 'was successful', buildVersion, false
+			}
+		}
+		catch(err) {
+			notify 'Failure', 'failed', buildVersion, true
+			throw err
+		}
+	}
+}
+
+def notify(status, bodyMessage, buildVersion, attachBuildLog) {
+	emailext subject: "${env.JOB_NAME} $buildVersion Build $status", body: "The ${env.JOB_NAME} $buildVersion build $bodyMessage", to: 'Dhwajad.Kulkarni@aig.com', mimeType: 'text/html', attachLog: attachBuildLog
+}
